80ccc0111c66bfd2553f1d44aaae079a95f78c1f
Dante Hernandez <hernandezdante2001@gmail.com>
2023-07-13 11:15:57 -0400

    updated base link

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/dhernandez39555/pokedex-reactDeploy",
+  base: "/pokedex-reactDeploy",
   plugins: [react()],
-  
+
 })
