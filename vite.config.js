4957b1682297e516f07f08309249e89b64897d40
Dante Hernandez <hernandezdante2001@gmail.com>
2023-07-13 10:49:30 -0400

    Built Github Actions Deploy Workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/dhernandez39555/pokedex-reactDeploy",
   plugins: [react()],
+  
 })
